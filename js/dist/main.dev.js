85e6ae791b918eb054e625b2af3fa0d9ebe2fbff
Ashu Tamar <69355116+Ashutamar5@users.noreply.github.com>
2021-04-22 11:21:09 +0530

    okay

---
+++ js/dist/main.dev.js
@@ -0,0 +1,19 @@
+// features carousel start
+// $('.features-carousel').owlCarousel({
+//     loop:true,
+//     margin:0,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,    
+//         },
+//         600:{
+//             items:2,   
+//         },
+//         1000:{
+//             items:3,   
+//         }
+//     }
+// })
+// features carousel end
+"use strict";
